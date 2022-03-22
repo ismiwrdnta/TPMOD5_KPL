4d94c03cf0f069d8f84d4a76c68715a654b7aec1
ismiwrdnta <ismiwrdnta@gmail.com>
2022-03-22 22:37:57 +0700

    menambahkan class SayaTubeVideo

--- tpmodul5_1302201135/tpmodul5_1302201135/Program.cs
+++ tpmodul5_1302201135/tpmodul5_1302201135/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace tpmodul5_1302201135
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            SayaTubeVideo newVid = new SayaTubeVideo("Tutorial Design By Contract - Ismi Wardanita");
+            newVid.IncreasePlayCount(3);
+            newVid.PrintVideoDetails();
+        }
+    }
+}
---
+++ tpmodul5_1302201135/tpmodul5_1302201135/SayaTubeVideo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace tpmodul5_1302201135
+{
+	public class SayaTubeVideo
+	{
+		private int id;
+		private string title;
+		private int playCount;
+		public SayaTubeVideo() { }
+		public SayaTubeVideo(string title)
+		{
+			this.title = title;
+			var rand = new Random();
+			this.id = rand.Next(10000, 99999);
+			this.playCount = 0;
+		}
+
+		public void IncreasePlayCount(int a)
+		{
+			this.playCount = a;
+		}
+
+		public void PrintVideoDetails()
+		{
+			Console.WriteLine("Video ID : " + this.id);
+			Console.WriteLine("Video Title : " + this.title);
+			Console.WriteLine("Video Playcount : " + this.playCount);
+		}
+
+
+	}
+}
