ffdf0756d3704802440e92fea2fef9b95f4005ee
ismiwrdnta <ismiwrdnta@gmail.com>
2022-03-22 23:30:00 +0700

    implementasi design by contract

--- tpmodul5_1302201135/tpmodul5_1302201135/Program.cs
+++ tpmodul5_1302201135/tpmodul5_1302201135/Program.cs
@@ -9,6 +9,15 @@ namespace tpmodul5_1302201135
             SayaTubeVideo newVid = new SayaTubeVideo("Tutorial Design By Contract - Ismi Wardanita");
             newVid.IncreasePlayCount(3);
             newVid.PrintVideoDetails();
+
+            Console.WriteLine();
+            newVid.IncreasePlayCount(1000);
+            newVid.PrintVideoDetails();
+
+            Console.WriteLine();
+            newVid.IncreasePlayCount(13000000);
+            newVid.PrintVideoDetails();
+
         }
     }
 }
--- tpmodul5_1302201135/tpmodul5_1302201135/SayaTubeVideo.cs
+++ tpmodul5_1302201135/tpmodul5_1302201135/SayaTubeVideo.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.Contracts;
 
 namespace tpmodul5_1302201135
 {
@@ -10,15 +11,28 @@ namespace tpmodul5_1302201135
 		public SayaTubeVideo() { }
 		public SayaTubeVideo(string title)
 		{
+			Contract.Requires(this.title != null);
+			Contract.Requires(this.title.Length <= 100);
 			this.title = title;
+
 			var rand = new Random();
 			this.id = rand.Next(10000, 99999);
+
 			this.playCount = 0;
 		}
 
 		public void IncreasePlayCount(int a)
-		{
-			this.playCount = a;
+		{ 
+			Contract.Requires(a <= 1000000);
+            try
+            {
+				if (a > 10000000) throw new Exception("Jumlah input play count melebihi 10.000.000!");
+				this.playCount = checked(this.playCount + a);	
+            }
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+            }
 		}
 
 		public void PrintVideoDetails()
